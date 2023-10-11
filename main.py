75fb1d39935d6b9e14d3a4c1f960bcc25b9cf9a5
INGAMIL <144364960+INGAMIL@users.noreply.github.com>
2023-10-11 21:50:54 +0300

    analizeS tesinys

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
 
 
 # is viso registracijos lentele 2021- 2022-2023
@@ -76,6 +79,8 @@ import matplotlib.pyplot as plt
 # ANALIZE
 # 2021 METAI IVEZTU I LT IS VISO:
 
+# TIKSLAS: PATIKRINTI, AR EGZISTUOJA AUTOMOBILIU PIRKIMO SEZONISKUMAS: 2021/2022
+
 # df = pd.read_csv('automobiliu_registracija_2021.csv', skipfooter=1, engine='python')
 # df['Is viso registruota'] = df['Is viso naudotu transporto priemoniu']+df['Is viso nauju transporto priemoniu']
 # naujas_df_2021 = df[['Menuo', 'Is viso registruota']].copy()
@@ -101,6 +106,11 @@ import matplotlib.pyplot as plt
 # plt.xticks(rotation=45)
 # plt.show()
 
+# ISVADA: LIETUVOJE DOMINUOJA I LIETUVA IVEZAMU PIRMA KARTA AUTOMOBILIU SEZONISKUMAS:
+# DAUGIAUSIAI AUTOMOBILIU REGISTRACIJU ATLIEKAMA PAVASARI IR VASAROS PRADZIOJE.
+
+#  STULPELINIS GRAFIKAS:
+
 # data_files = [('automobiliu_registracija_2021.csv', '2021'),
 #               ('automobiliu_registracija_2022.csv', '2022')]
 # plt.figure(figsize=(12, 8))
@@ -120,6 +130,11 @@ import matplotlib.pyplot as plt
 # plt.tight_layout()
 # plt.show()
 
+# ISVADA: PALYGINUS 2021-2022 METUS, MATYTI, KAD REGISTRUOJAMU AUTOMOBILIU SEZONISKUMO TENDENCIJA
+# ISLIEKA PANASI.
+
+ # LINIJINIS GRAFIKAS:
+
 # data_files = [('automobiliu_registracija_2021.csv', '2021'),
 #               ('automobiliu_registracija_2022.csv', '2022')]
 # plt.figure(figsize=(12, 8))
@@ -135,6 +150,29 @@ import matplotlib.pyplot as plt
 # plt.legend()
 # plt.tight_layout()
 # plt.show()
+
+# DVIEJU METU DUOMENYS STULPELINIUOSE GRAFIKUOSE ATVAIZDUOJAMI VIENAME LAPE:
+
+# data_files = [('automobiliu_registracija_2021.csv', '2021'),
+#               ('automobiliu_registracija_2022.csv', '2022')]
+# plt.figure(figsize=(12, 8))
+# for index, (data_file, year) in enumerate(data_files, 1):
+#     df = pd.read_csv(data_file, skipfooter=1, engine='python')
+#     df['Is viso registruota'] = df['Is viso naudotu transporto priemoniu'] + df['Is viso nauju transporto priemoniu']
+#     naujas_df = df[['Menuo', 'Is viso registruota']].copy()
+#     plt.subplot(2, 1, index)
+#     plt.bar(naujas_df['Menuo'], naujas_df['Is viso registruota'])
+#     plt.xlabel('Menuo')
+#     plt.ylabel('Is viso registruota')
+#     plt.title(f'Transporto registracija {year}')
+#     plt.xticks(rotation=45)
+#     plt.tight_layout()
+# plt.show()
+# ISVADA: VIZUALIZACIJAI IR 2021/2022 METU PALYGINIMUI TINKAMAS IR STULPELINIS, IR LINIJINIS GRAFIKAS.
+
+
+# TIKSLAS: PALYGINTI, KIEK BUVO IS VISO REGISTRUOTA NAUJU IR
+# NAUDOTU TRANSPORTO PRIEMONIU 2021 IR 2022 METAIS
 
 # data_files = [('automobiliu_registracija_2021.csv', '2021'),
 #               ('automobiliu_registracija_2022.csv', '2022')]
@@ -155,7 +193,11 @@ import matplotlib.pyplot as plt
 # plt.tight_layout()
 # plt.show()
 
-# naujų automobilių procentinė dalis nuo viso registruotų automobilių kiekvienais metais.
+# ISVADA: ANALIZUOJANT PAMENESIUI MATYTI, KAD I LIETUVA IVEZAMA  BEVEIK TRIGUBAI DAUGIAU
+# NAUDOTU, NEI NAUJU TRANSPORTO PRIEMONIU
+
+# TIKSLAS: NUSTATYTI, KOKIA YRA NAUJU AUTOMOBILIU PROCENTINE DALIS
+# NUO VISU REGISTRUOTU AUTOMOBILIU 2021/2022 METAIS
 
 # data_files = [('automobiliu_registracija_2021.csv', '2021'),
 #               ('automobiliu_registracija_2022.csv', '2022')]
@@ -166,7 +208,7 @@ import matplotlib.pyplot as plt
 #     viso = df['Is viso naudotu transporto priemoniu'].sum() + nauji
 #     procentine_dalis = (nauji / viso) * 100
 #     procentai.append(procentine_dalis)
-# # Nubraizome grafiką
+#
 # plt.bar([year for _, year in data_files], procentai, color=['blue', 'green'])
 # plt.xlabel('Metai')
 # plt.ylabel('Procentai')
@@ -174,21 +216,8 @@ import matplotlib.pyplot as plt
 # plt.ylim(0, 100)  # Nustatome Y ašies ribas nuo 0 iki 100
 # plt.show()
 
-# data_files = [('automobiliu_registracija_2021.csv', '2021'),
-#               ('automobiliu_registracija_2022.csv', '2022')]
-# plt.figure(figsize=(12, 8))
-# for index, (data_file, year) in enumerate(data_files, 1):
-#     df = pd.read_csv(data_file, skipfooter=1, engine='python')
-#     df['Is viso registruota'] = df['Is viso naudotu transporto priemoniu'] + df['Is viso nauju transporto priemoniu']
-#     naujas_df = df[['Menuo', 'Is viso registruota']].copy()
-#     plt.subplot(2, 1, index)
-#     plt.bar(naujas_df['Menuo'], naujas_df['Is viso registruota'])
-#     plt.xlabel('Menuo')
-#     plt.ylabel('Is viso registruota')
-#     plt.title(f'Transporto registracija {year}')
-#     plt.xticks(rotation=45)
-#     plt.tight_layout()
-# plt.show()
+# ISVADA: ANALIZUOJANT METINIUS DUOMENIS MATYTI, KAD I LIETUVA IVEZAMA  TIK TRECDALIS NAUJU TRANSPORTO
+# PRIEMONIU NUO VISU PER METUS IVEZAMU TRANSPORTO PRIEMONIU
 
 
 
@@ -324,18 +353,119 @@ import matplotlib.pyplot as plt
 #
 
 # ANALIZE NAUJU AUTOMOBILIU POPULIARUMO 2021 VS 2022:
-# nebaigta!!!!!
-# data_file = [('populiariausios_naujos_markes_2021.csv', '2021')
-# plt.figure(figsize=(12, 8))
-# for data_file, year in data_files:
-#     df = pd.read_csv(data_file, skipfooter=1, engine='python')
-#     df['Is viso registruota'] = df['Is viso naudotu transporto priemoniu'] + df['Is viso nauju transporto priemoniu']
-#     plt.plot(df['Menuo'], df['Is viso registruota'], marker='o', label=f'{year}')
-# plt.xlabel('Menuo')
-# plt.ylabel('Is viso registruota')
-# plt.title('Transporto registracija 2021 vs 2022')
+
+# prie nauju populiauriusiu markiu 2021 pridejome stulpeli su visais pirkimais pamenesiui is automobiliu registracijos 2021
+
+
+# populiariausios_naujos_markes_2021_new = pd.read_csv('populiariausios_naujos_markes_2021.csv')
+# automobiliu_registracija_2021 = pd.read_csv('automobiliu_registracija_2021.csv')
+# nauji_automobiliai_M1 = automobiliu_registracija_2021['Is ju M1 klases nauju lengvuju automobiliu']
+# populiariausios_naujos_markes_2021_new['Is ju M1 klases nauju lengvuju automobiliu'] = nauji_automobiliai_M1
+# populiariausios_naujos_markes_2021_new.to_csv('populiariausios_naujos_markes_2021_new.csv', index=False)
+
+# prie nauju populiauriusiu markiu 2022 pridejome stulpeli su visais pirkimais pamenesiui is automobiliu registracijos 2021
+
+
+# populiariausios_naujos_markes_2022_new = pd.read_csv('populiariausios_naujos_markes_2022.csv')
+# automobiliu_registracija_2022 = pd.read_csv('automobiliu_registracija_2022.csv')
+# nauji_automobiliai_M1_2022 = automobiliu_registracija_2022['Is ju M1 klases nauju lengvuju automobiliu']
+# populiariausios_naujos_markes_2022_new['Is ju M1 klases nauju lengvuju automobiliu'] = nauji_automobiliai_M1_2022
+# populiariausios_naujos_markes_2022_new.to_csv('populiariausios_naujos_markes_2022_new.csv', index=False)
+
+
+
+
+# Procentinė populiariausiu M1 klases nauju automobiliu dalis nuo visu nauju registruotu 2021
+
+# df = pd.read_csv('populiariausios_naujos_markes_2021_new.csv', skipfooter=1, engine='python')
+# df = df[df['Unnamed: 0'] != 'Iš viso']
+# df['FIAT'] = df['FIAT'].astype(int)
+# df['TOYOTA'] = df['TOYOTA'].astype(int)
+# df['VOLKSWAGEN'] = df['VOLKSWAGEN'].astype(int)
+# df['SKODA'] = df['SKODA'].astype(int)
+# df['PEUGEOT'] = df['PEUGEOT'].astype(int)
+# df['FIAT_procentai'] = (df['FIAT'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# df['TOYOTA_procentai'] = (df['TOYOTA'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# df['VOLKSWAGEN_procentai'] = (df['VOLKSWAGEN'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# df['SKODA_procentai'] = (df['SKODA'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# df['PEUGEOT_procentai'] = (df['PEUGEOT'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# plt.figure(figsize=(10, 6))
+# plt.plot(df['Mėnuo'], df['FIAT_procentai'], label='FIAT', marker='o')
+# plt.plot(df['Mėnuo'], df['TOYOTA_procentai'], label='TOYOTA', marker='o')
+# plt.plot(df['Mėnuo'], df['VOLKSWAGEN_procentai'], label='VOLKSWAGEN', marker='o')
+# plt.plot(df['Mėnuo'], df['SKODA_procentai'], label='SKODA', marker='o')
+# plt.plot(df['Mėnuo'], df['PEUGEOT_procentai'], label='PEUGEOT', marker='o')
+# plt.xlabel('Mėnuo')
+# plt.ylabel('Procentai nuo visų naujų M1 automobilių')
+# plt.title('Procentinė populiariausiu M1 klases nauju automobiliu dalis nuo visu nauju registruotu 2021 m.')
 # plt.xticks(rotation=45)
-# plt.grid(True, which='both', linestyle='--', linewidth=0.5)  # pridedame tinklelį grafičiui
 # plt.legend()
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 # plt.tight_layout()
 # plt.show()
+
+
+# Procentinė populiariausiu M1 klases nauju automobiliu dalis nuo visu nauju registruotu 2022
+# #
+# df = pd.read_csv('populiariausios_naujos_markes_2022_new.csv',skipfooter=1, engine='python')
+# df = df[df['Unnamed: 0'] != 'Iš viso']
+#
+# # 2022 m populiariausiu automobiliu markes skiriasi
+#
+# df['TOYOTA'] = df['TOYOTA'].astype(int)
+# df['VOLKSWAGEN'] = df['VOLKSWAGEN'].astype(int)
+# df['SKODA'] = df['SKODA'].astype(int)
+# df['KIA'] = df['KIA'].astype(int)
+# df['NISSAN'] = df['NISSAN'].astype(int)
+# df['TOYOTA_procentai'] = (df['TOYOTA'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# df['VOLKSWAGEN_procentai'] = (df['VOLKSWAGEN'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# df['SKODA_procentai'] = (df['SKODA'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# df['KIA_procentai'] = (df['KIA'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# df['NISSAN_procentai'] = (df['NISSAN'] / df['Is ju M1 klases nauju lengvuju automobiliu']) * 100
+# plt.figure(figsize=(10, 6))
+#
+# plt.plot(df['Mėnuo'], df['TOYOTA_procentai'], label='TOYOTA', marker='o')
+# plt.plot(df['Mėnuo'], df['VOLKSWAGEN_procentai'], label='VOLKSWAGEN', marker='o')
+# plt.plot(df['Mėnuo'], df['SKODA_procentai'], label='SKODA', marker='o')
+# plt.plot(df['Mėnuo'], df['KIA_procentai'], label='KIA', marker='o')
+# plt.plot(df['Mėnuo'], df['NISSAN_procentai'], label='NISSAN', marker='o')
+# plt.xlabel('Mėnuo')
+# plt.ylabel('Procentai nuo visų naujų M1 automobilių')
+# plt.title('Procentinė populiariausiu M1 klases nauju automobiliu dalis nuo visu nauju registruotu 2022 m.')
+# plt.xticks(rotation=45)
+# plt.legend()
+# plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+# plt.tight_layout()
+# plt.show()
+#
+# TIKSLAS: ATLIKTI NAUJU IVEZAMU TRANSPORTO PRIEMONIU PROGNOZE ATEINANTIEMS 5 METAMS
+#
+# data = {
+#     'Metai': [2018, 2019, 2020, 2021, 2022],
+#     'Automobiliu_sk': [60179, 72589, 59560, 61475, 56498]
+# }
+# df = pd.DataFrame(data)
+# # Paruošiame duomenis modeliui
+# X = df['Metai'].values.reshape(-1,1)
+# y = df['Automobiliu_sk'].values
+# # Apmokome modelį
+# model = LinearRegression().fit(X, y)
+# # Gaukime prognozę ateinantiems 5 metams
+# ateinanciu_metu_skaicius = 5
+# ateinanciu_metu_prognoze = np.array(range(2023, 2023 + ateinanciu_metu_skaicius)).reshape(-1, 1)
+# predicted_values = model.predict(ateinanciu_metu_prognoze)
+# # Pavaizduokime istorinius duomenis ir prognozę grafike
+# plt.figure(figsize=(10, 6))
+# plt.scatter(df['Metai'], df['Automobiliu_sk'], color='blue', label='Istoriniai duomenys')
+# plt.plot(ateinanciu_metu_prognoze, predicted_values, color='red', linestyle='-', marker='o', label='Prognozė')
+# plt.xlabel('Metai')
+# plt.ylabel('Automobilių skaičius')
+# plt.title('Naujų automobilių įvežimo prognozė')
+# plt.legend()
+# plt.grid(True)
+# plt.show()
+
+# ISVADA: NAUDOJANT TIESINES REGRESIJOS PROGNOZAVIMO METODA, MATYTI, KAD ISLIEKA
+# NAUJU TRANSPORTO PRIEMONIU IVEZIMO MAZEJIMO TENDENCIJA. ISTORISKAI RODIKLIAMS ITAKOS
+# TUREJO: PANDEMIJA, PUSLAIDININKIU DEFICITAS, UKRAINOS KARAS, EURIBOR KILIMAS
+# IR INFLIACIJA
